0e4d6c737c0382faf83281443a3c354f91d96dee
Víctor Pérez <victorperezvl95@gmail.com>
2025-02-11 13:55:55 +0100

    Creación del primer archivo estático en el frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,24 @@
-const express = require ('express');
+const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes.js');
+const path = require('path');
 
 //Initial configuration
 const app = express();
 app.set('port', 4000);
 app.listen(app.get('port'));
 
+// Statics files
+app.use(express.static(path.join(__dirname,'../../frontend')));
+
 //Middlewares
 app.use(morgan('dev'));
 
 //Routes
 app.use('/api', routes);
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname,'../../frontend', 'index.html'))
+});
 
 
 console.log('probando');
