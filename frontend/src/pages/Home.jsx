5d7fe3a472ce640098c88307e571834d62355962
Víctor Pérez <victorperezvl95@gmail.com>
2025-02-26 12:22:35 +0100

    Primeros complementos frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
+import "../styles/Home.css";
+import userIcon from "../assets/iconUser.png"; 
 
 const Home = () => {
   const [user] = useState(null); // Aquí guardarás los datos del usuario cuando hagas login
 
   return (
-    <div className="home-container">
-      <h1>Bienvenido a Yamaki</h1>
-      {user ? (
-        <p>Hola, {user.name}! Aquí verás tus citas.</p>
-      ) : (
-        <p>Inicia sesión para ver tus citas.</p>
-      )}
+    <div>
+       <nav className="navbar">
+        <div className="logo">Mi Sitio</div>
+          <div className="menu-container">
+            <div className="menu">
+                <a href="#inicio">Inicio</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#servicios">Galería</a>
+                <a href="#contacto">Contacto</a>
+                <a href="#servicios">CITA</a>
+            </div>
+                <img src={userIcon} alt="Usuario" className="user-icon" />
+          </div>
+        </nav>
+      <div className="home-container">
+        <h1>Bienvenido a Yamaki</h1>
+        {user ? (
+          <p>Hola, {user.name}! Aquí verás tus citas.</p>
+        ) : (
+          <p>Inicia sesión para ver tus citas.</p>
+        )}
+      </div>
     </div>
   );
 };
