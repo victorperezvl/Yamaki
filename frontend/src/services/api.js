9dad488571f3e0b3512db5fd63da16c95ee6e70a
Víctor Pérez <victorperezvl95@gmail.com>
2025-03-06 17:41:42 +0100

    implementado fetch para reservar cita

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 const API_URL = import.meta.env.VITE_URL_API;
 
+//Fetch for get hairdressers
 export const fetchHairdressers = async () => {
     try {
         const response = await fetch (`${API_URL}/hairdresser`);
@@ -15,6 +16,7 @@ export const fetchHairdressers = async () => {
     
 }
 
+//Fetch for get services
 export const fetchServices = async () => {
     try {
         const response = await fetch (`${API_URL}/services`);
@@ -29,12 +31,36 @@ export const fetchServices = async () => {
     }
 }
 
-export const fetchAppointment = async () => {
-    const appointmentData = {
-        hairdresser: selectedHairdresser,
-        service: selectedService,
-        
-    }
+//Fetch for book appointment for guest users
+export const fetchBookGuest = async (email, phone, name, hairdresser, date, time) => {
+    const bookData = {
 
-}
+        email,
+        phone,
+        name,
+        hairdresser,
+        date,
+        time
+    }
+    try {
+        const response = await fetch ('/api/appointments/guest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bookData),
+        })
+    
+        if (response.ok) {
+            alert('Cita confirmada');
+          } else {
+            alert('Error al crear la cita');
+          }
+
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+        alert('Hubo un problema al conectar con el servidor.');
+    }        
+
+};
 
